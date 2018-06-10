32665fe1cc11b9ba438d81d6dfbe1e765ff8cc75
Irina Proekt <pr_o@inbox.ru>
2018-06-10 21:00:42 +0300

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,12 @@
+let d = process.argv[2];
+let m = process.argv[3];
+let y = process.argv[4];
+
+let month = {'января': 0, 'февраля': 1, 'марта': 2, 'апреля': 3, 'мая': 4, 'июня': 5, 'июля': 6, 'августа': 7, 'сентября': 8, 'октября': 9, 'ноября': 10, 'декабря': 11};
+
+let now = new Date(y, month[m], d);			 
+let newYear = new Date(y + 1, 0, 0);
+
+let period = (newYear - now) / (3600 * 24 * 1000);
+
+process.stdout.write(period);
